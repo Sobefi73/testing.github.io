9b382d5f4b9d54e9e0a82e9f97fe5ffdad508cd6
Sobefi73 <51924786+Sobefi73@users.noreply.github.com>
2019-10-26 05:20:31 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    nav : false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+
+})
+
